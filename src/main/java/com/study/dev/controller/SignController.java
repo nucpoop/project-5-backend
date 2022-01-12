498e2967a21683e428dd0d21b447a53ae5cb67d7
nucpoop <kjm5546@gmail.com>
2022-01-12 17:36:47 +0900

    add sign in controller

--- src/main/java/com/study/dev/controller/SignController.java
+++ src/main/java/com/study/dev/controller/SignController.java
@@ -1,8 +1,35 @@
 package com.study.dev.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.study.dev.model.Sign;
+import com.study.dev.model.User;
+import com.study.dev.security.JwtTokenProvider;
+import com.study.dev.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SignController {
     
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping(value = "/signin")
+    @ResponseBody
+    public Sign signInUser(HttpServletRequest request, @RequestBody User user){
+        //User result = (User)userService
+        return null;
+    }
 }
