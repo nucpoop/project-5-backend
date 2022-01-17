8531a993ef56888c74eb1a3bd0ff1b8377fce826
김재민 <kjm5546@gmail.com>
2022-01-17 15:59:12 +0900

    add default

--- src/main/java/com/study/dev/model/Default.java
+++ src/main/java/com/study/dev/model/Default.java
@@ -1,5 +1,24 @@
 package com.study.dev.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Default {
     
+    @Column(nullable = false)
+    private Long insertId = 0L;
+
+    private Date insertTime;
+
+    private Long updateId = 0L;
+
+    private Date updateTime;
 }
