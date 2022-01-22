bc0ed8d774b49a98501898a9f2a9dde8b8b6ad03
김재민 <kjm5546@gmail.com>
2022-01-23 02:33:44 +0900

    trivial clean import

--- src/main/java/com/study/dev/model/User.java
+++ src/main/java/com/study/dev/model/User.java
@@ -13,11 +13,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.NoArgsConstructor;
 
 @Builder
 @Entity
