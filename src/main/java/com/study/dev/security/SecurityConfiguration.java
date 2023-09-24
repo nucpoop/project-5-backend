fab7c1b54490becb8a1dffc4d961954b25bc9d98
nucpoop <kjm5546@gmail.com>
2023-09-24 14:38:40 +0900

    feat: swagger 추가

--- src/main/java/com/study/dev/security/SecurityConfiguration.java
+++ src/main/java/com/study/dev/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +23,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/v3/api-docs",  "/configuration/ui",
+                "/swagger-resources", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**","/swagger/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.httpBasic().disable()                                                      //rest api 서비스 이므로 기본설정 사용안함
@@ -33,7 +41,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/user/sign-in"
                         , "/user/sign-up"
                         , "/index"
-                        , "/health-check")
+                        , "/health-check"
+                        , "/swagger-ui/**"
+                        , "/swagger-resources/**"
+                )
                 .permitAll()
                 .anyRequest().hasRole("USER")
                 .and()
